3b91b22137f2c5d51f54da490a772e62a9f660f5
Bill-GD <geometrydashbill@gmail.com>
2025-09-01 15:50:37 +0700

    added env to root layout context

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,11 @@
+import { API_PORT, API_SERVER } from '$env/static/private';
+import type { LayoutServerLoad } from './$types';
+
+export const load: LayoutServerLoad = async () => {
+  return {
+    env: {
+      API_SERVER,
+      API_PORT,
+    },
+  };
+};
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
   import '../app.css';
   import 'tippy.js/dist/tippy.css';
+  import type { LayoutProps } from './$types';
+  import { setContext } from 'svelte';
 
-  let { children } = $props();
+  let { children, data }: LayoutProps = $props();
+
+  setContext('API_URL', `${data.env.API_SERVER}:${data.env.API_PORT}`);
 </script>
 
-{@render children()}
+<main class="flex h-dvh items-center justify-center bg-gray-950">
+  {@render children()}
+</main>
