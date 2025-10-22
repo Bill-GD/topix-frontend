a04bd3f3bff6aea54296926fd5881b93c7d4c06b
Bill-GD <geometrydashbill@gmail.com>
2025-10-22 10:02:44 +0700

    added search for users page

--- src/routes/(app)/users/+page.server.ts
+++ src/routes/(app)/users/+page.server.ts
@@ -3,8 +3,9 @@ import { CookieName, type User } from '$lib/utils/types';
 import { type Actions, fail } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ fetch }) => {
-  const res = await fetch(`/api/users`);
+export const load: PageServerLoad = async ({ fetch, url }) => {
+  const searchString = url.searchParams.get('q');
+  const res = await fetch(`/api/users${searchString ? `?name=${searchString}` : ''}`);
   return {
     users: (await res.json()) as unknown as User[],
     endOfList: res.headers.get('x-end-of-list') === 'true',
--- src/routes/(app)/users/+page.svelte
+++ src/routes/(app)/users/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import { enhance } from '$app/forms';
+  import { goto } from '$app/navigation';
+  import { page } from '$app/state';
   import { Button, IconButton } from '$lib/components/button';
+  import { Input } from '$lib/components/input';
   import { Scroller } from '$lib/components/layout';
   import { Icon, ReturnHeader } from '$lib/components/misc';
   import { Modal, ModalBody, ModalFooter, ModalHeader } from '$lib/components/modal';
@@ -16,6 +19,8 @@
   let pageIndex = 1;
   let disableScroller = $state<boolean>(data.endOfList);
   let users = $derived(data.users);
+  let searchString = $derived<string>(decodeURIComponent(page.url.searchParams.get('q') ?? ''));
+  let searched = $derived<boolean>(page.url.searchParams.has('q'));
 
   function hideModal() {
     showModal = null;
@@ -29,43 +34,83 @@
 <ReturnHeader>Users</ReturnHeader>
 
 <div class="flex flex-col gap-4">
-  {#each users as user}
-    <a
-      class="flex items-center gap-4 box hover:bg-zinc-100 dark:hover:bg-zinc-800/80"
-      href="/user/{user.username}"
+  <form
+    class="relative"
+    method="post"
+    onsubmit={(ev) => {
+      ev.preventDefault();
+      goto(`?q=${encodeURIComponent(searchString)}`, { replaceState: true });
+    }}
+  >
+    <Input
+      name="search-string"
+      bind:value={searchString}
+      clearable
+      placeholder="Search by username or display name"
     >
-      <img
-        class="profile-picture-md"
-        src={user.profilePicture ?? '/images/default-user-profile-icon.jpg'}
-        alt="profile"
-      />
-      <div class="flex flex-col gap-2">
-        <p class="text-gray-700">#{user.id}</p>
-        <div class="flex items-baseline gap-2">
-          <span class="text-xl font-semibold">{user.displayName}</span>
-          <span class="text-gray-500">@{user.username}</span>
-        </div>
-        <p>Role: {user.role}</p>
-      </div>
+      {#snippet prefixIcon()}
+        <Icon class="text-zinc-500" type="search" size="sm" />
+      {/snippet}
+    </Input>
+  </form>
 
-      {#if user.role !== 'admin'}
-        <IconButton
-          class="ml-auto p-2"
-          onclick={() => {
-            showModal = 'delete';
-            username = user.username;
-          }}
-        >
-          <Icon type="delete" class="text-red-500" />
-        </IconButton>
-      {/if}
-    </a>
-  {/each}
+  {#if searched}
+    <Button
+      class="mr-auto"
+      type="dark"
+      outline
+      onclick={() => {
+        goto(`/users`, { replaceState: true });
+      }}
+    >
+      Clear result
+    </Button>
+  {/if}
+
+  {#if searched && users.length <= 0}
+    <p class="p-4 text-center text-xl font-semibold">No result found.</p>
+  {:else}
+    {#each users as user}
+      <a
+        class="flex items-center gap-4 box hover:bg-zinc-100 dark:hover:bg-zinc-800/80"
+        href="/user/{user.username}"
+      >
+        <img
+          class="profile-picture-md"
+          src={user.profilePicture ?? '/images/default-user-profile-icon.jpg'}
+          alt="profile"
+        />
+        <div class="flex flex-col gap-2">
+          <p class="text-gray-700">#{user.id}</p>
+          <div class="flex items-baseline gap-2">
+            <span class="text-xl font-semibold">{user.displayName}</span>
+            <span class="text-gray-500">@{user.username}</span>
+          </div>
+          <p>Role: {user.role}</p>
+        </div>
+
+        {#if user.role !== 'admin'}
+          <IconButton
+            class="ml-auto p-2"
+            onclick={() => {
+              showModal = 'delete';
+              username = user.username;
+            }}
+          >
+            <Icon type="delete" class="text-red-500" />
+          </IconButton>
+        {/if}
+      </a>
+    {/each}
+  {/if}
 
   <Scroller
     disabled={disableScroller}
+    hideText={searched && users.length <= 0}
     attachmentCallback={async () => {
-      const res = await fetch(`/api/users?page=${++pageIndex}`);
+      const res = await fetch(
+        `/api/users?page=${++pageIndex}${searched ? `&name=${searchString}` : ''}`,
+      );
       const newData = await res.json();
       disableScroller = res.headers.get('x-end-of-list') === 'true';
       users = [...users, ...newData];
