bf7843decaccde0b5c9b5d8df245276aaf94ab50
Bill-GD <geometrydashbill@gmail.com>
2025-11-17 11:10:30 +0700

    added notification deletion
    fixed user overview preload

--- src/lib/components/overview/UserOverview.svelte
+++ src/lib/components/overview/UserOverview.svelte
@@ -17,33 +17,36 @@
   }
 </script>
 
-<a class="flex items-center gap-4 box box-hover" href="/user/{user.username}">
-  <img
-    class="profile-picture-md"
-    src={user.profilePicture ?? '/images/default-user-profile-icon.jpg'}
-    alt="profile"
-  />
-  <div class="flex flex-col gap-2">
-    <p class="text-gray-700">#{user.id}</p>
-    <div class="flex items-baseline gap-2">
-      <span class="text-xl font-semibold">{user.displayName}</span>
-      <span class="text-gray-500">@{user.username}</span>
+<div class="relative">
+  <a class="absolute inset-0 z-0" href="/user/{user.username}" aria-label="link"></a>
+  <div class="flex items-center gap-4 box box-hover">
+    <img
+      class="profile-picture-md"
+      src={user.profilePicture ?? '/images/default-user-profile-icon.jpg'}
+      alt="profile"
+    />
+    <div class="flex flex-col gap-2">
+      <p class="text-gray-700">#{user.id}</p>
+      <div class="flex items-baseline gap-2">
+        <span class="text-xl font-semibold">{user.displayName}</span>
+        <span class="text-gray-500">@{user.username}</span>
+      </div>
+      <p>Role: {user.role}</p>
     </div>
-    <p>Role: {user.role}</p>
-  </div>
 
-  {#if user.role !== 'admin'}
-    <Button
-      class="ml-auto p-2 dark:hover:bg-zinc-700"
-      onclick={(ev) => {
-        ev.preventDefault();
-        showModal = true;
-      }}
-    >
-      <Icon type="delete" class="text-red-500" />
-    </Button>
-  {/if}
-</a>
+    {#if user.role !== 'admin'}
+      <Button
+        class="z-1 ml-auto p-2 dark:hover:bg-zinc-700"
+        onclick={(ev) => {
+          ev.preventDefault();
+          showModal = true;
+        }}
+      >
+        <Icon type="delete" class="text-red-500" />
+      </Button>
+    {/if}
+  </div>
+</div>
 
 <Modal show={showModal} backdropCallback={hideModal} center>
   <ModalHeader>Delete account</ModalHeader>
